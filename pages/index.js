0c0dc38348e8f52139f87b8164790b6db6b86e78
Redion Kadilliu <rkadilliu@gmail.com>
2020-11-29 22:40:14 -0500

    added fetch function

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
+const [queriedMovies, setQueriedMovies] = useState([]);
 
 export default function Home() {
   const searchMovies = async (e) => {
     e.preventDefault();
-    const query = "Bruce";
-
-    fetch();
+    console.log("submitting");
+    const query = "Spider Man";
+    let URL = `https://api.themoviedb.org/3/search/movie?api_key=bb0fa06f949fe0af4ffdc970ba8e73c7&language=en-US&query=${query}&page=1&include_adult=false`;
+    fetch(URL).then((res) =>
+      res.json().then((json) => setQueriedMovies(json.results))
+    );
   };
   return (
     <div className={styles.container}>
@@ -35,6 +40,13 @@ export default function Home() {
             Search
           </button>
         </form>
+        {queriedMovies.map((queriedMovie) => (
+          <div
+            style={{ borderStyle: "solid", width: "200px", height: "200px" }}
+          >
+            {queriedMovie}
+          </div>
+        ))}
       </main>
     </div>
   );
